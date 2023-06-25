398248d26877aca2718e8005bea85b0f164c175e
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-06-25 20:44:55 +0700

    Add Boolean

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main() {
+
+	fmt.Println("Hello World!")
+	fmt.Println("Anjaaz kelazz!")
+
 	fmt.Println("Satu = ", 1)
 	fmt.Println("Dua", 2)
 	fmt.Println("Tiga Koma Lima", 3.5)
 
-	fmt.Println("Hello World!")
-	fmt.Println("Anjaaz kelazz!")
+	fmt.Println("Benar :", true)
+	fmt.Println("Salah :", false)
+
 }
