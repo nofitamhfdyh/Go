d158020f09eb9ccc7819d233527da71075e55c04
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-06-25 21:14:10 +0700

    Add Variable

--- main.go
+++ main.go
@@ -17,4 +17,35 @@ func main() {
 	fmt.Println(len("Nofita"))          //untuk menghitung jumlah karakter dari string
 	fmt.Println("Nofita Mahfudiyah"[0]) //hasilnya adalah representasi dari byte huruf N
 	fmt.Println("Nofita Mahfudiyah"[1]) //hasilnya adalah representasi dari byte huruf O
+
+	//(1)saat membuat variabel, wajib menuliskan tipe data dari variable
+	var name string
+
+	name = "Nofita Mahfudiyah"
+	fmt.Println(name)
+
+	name = "Farhan Rizky Alkarim" //value variable bisa diubah dlm golang
+	fmt.Println(name)
+
+	//(2)jika langsung menginisialisasi data pada variable, tidak wajib menuliskan tipe data
+	var nama = "Nupnup" //sudah diidentifikasi sendiri dengan golang tipe data berdasarkan value yang diinputkan
+	fmt.Println(nama)
+
+	var age = 45
+	fmt.Println(age)
+
+	country := "Indonesia" //kata kunci 'var' tidak wajib, jika langsung diinisialisasi datanya -> namun memakai := pada awal inisialisasi
+	fmt.Println(country)
+
+	country = "Nopnop" //jika mau dipakai lagi, cukup pakai =, tidak perlu :=
+	fmt.Println(country)
+
+	//deklarasi nultiple variable
+	var (
+		firstName = "Nofita"
+		lastName  = "Mahfudiyah"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
 }
