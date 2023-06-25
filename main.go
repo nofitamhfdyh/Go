fe758c4db6e09c2e8d1a6d0eaca76ae7df76eca3
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-06-25 20:53:04 +0700

    Add Function len dan index pada tipe data String

--- main.go
+++ main.go
@@ -14,4 +14,7 @@ func main() {
 	fmt.Println("Benar :", true)
 	fmt.Println("Salah :", false)
 
+	fmt.Println(len("Nofita"))          //untuk menghitung jumlah karakter dari string
+	fmt.Println("Nofita Mahfudiyah"[0]) //hasilnya adalah representasi dari byte huruf N
+	fmt.Println("Nofita Mahfudiyah"[1]) //hasilnya adalah representasi dari byte huruf O
 }
