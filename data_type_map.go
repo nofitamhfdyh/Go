1d14f6e673f2ee0de6c7126106c57b0703b74ea7
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-06-28 16:03:38 +0700

    Add Data Type Map

---
+++ data_type_map.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	person := map[string]string{ // [string] -> tipe key ; string{} -> tipe value
+		"name":    "Nofita",
+		"address": "Blitar",
+	}
+
+	person["title"] = "Technical Writer" // nambah data di map
+
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["address"])
+
+	book := make(map[string]string)
+	book["title"] = "Buku Go-Lang"
+	book["author"] = "NOfita Mahfudiyah"
+	book["wrong"] = "Ups"
+	fmt.Println(book)
+	fmt.Println(len(book))
+
+	delete(book, "wrong")
+	fmt.Println(book)
+	fmt.Println(len(book))
+
+}
