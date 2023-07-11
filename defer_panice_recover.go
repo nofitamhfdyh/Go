68b3f25651859a963c71fb4b1ea7a6cc30e2275d
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-11 09:15:33 +0700

    Tanpa pakai defer dan error

--- defer_panice_recover.go
+++ defer_panice_recover.go
@@ -1,16 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func logging() {
 	fmt.Println("Selesai memanggil function")
 }
 
-func runApplication() {
-	defer logging() // setelah function runApplication dieksekusi, func Logging pasti dieksekusi dipaling akhir walaupun penempatannya diawal, karena pakai defer. tpi pemakaian defer diusahakan diawal, jadi ketika didalam func terjadi error, tidak kena dampak
+func runApplication(value int) {
+	// defer logging() // setelah function runApplication dieksekusi, func Logging pasti dieksekusi dipaling akhir walaupun penempatannya diawal, karena pakai defer. tpi pemakaian defer diusahakan diawal, jadi ketika didalam func terjadi error, tidak kena dampak
 	fmt.Println("Run Application")
+	result := 10 / value
+	fmt.Println("Result ", result)
 }
 func main() {
 	//defer func = penjadwalan eksekusi function setelah function lain dieksekusi. akan tetap dieksekusi walaupun func yg dieksekusi sebelumnya error
-	runApplication()
+	runApplication(0)
 }
