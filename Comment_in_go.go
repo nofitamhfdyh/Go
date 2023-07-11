c05d68bdbb0529add7c8377a9d07d24d1dc03f37
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-11 10:09:22 +0700

    penggunaan Comment

---
+++ Comment_in_go.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+*
+komen
+banyak
+tak terbatas
+tinggal ketik ataasnya aja, ketika dienter, langsung otomatis digolan
+*/
+func main() {
+	var int int //klik int terakhir dengan menekan tombol command = utk membuka dokuemntasi
+	// komen 1 line
+}
