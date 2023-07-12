f81a5f62b69b01a1a5af64d558c88d54a05e1b8c
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-12 08:58:53 +0700

    mengembalikan struct tdk error

--- struct.go
+++ struct.go
@@ -6,7 +6,7 @@ import "fmt"
 type Customer struct { //utk struct, rata" namanya diawalai dengan huruf kapital. kalau ada 2 kata, brti 2 huruf paling depan kapital
 	Nama, Alamat string
 	Umur         int
-	Status       bool
+	//Status       bool
 }
 
 func main() {
