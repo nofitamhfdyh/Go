423de48749004802510ce5e2b676f26445a93ee5
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-12 08:55:43 +0700

    Struct dan struct literals OK

--- struct.go
+++ struct.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// (1)
 type Customer struct { //utk struct, rata" namanya diawalai dengan huruf kapital. kalau ada 2 kata, brti 2 huruf paling depan kapital
 	Nama, Alamat string
 	Umur         int
@@ -10,14 +11,28 @@ type Customer struct { //utk struct, rata" namanya diawalai dengan huruf kapital
 func main() {
 	// mmenggabungkan nol / lebih data yang tipenya beda dalam 1 kesatuan.
 	// sangat cocok utk merepresentasikan data daripada memakai map atau array, karena strukturnya lebih bagus
-	var me Customer
-	me.Nama = "Nofita Mahfudiyah"
-	me.Alamat = "Blitar"
-	me.Umur = 23
+	//(1)
+	var nupnup Customer
+	nupnup.Nama = "Nofita Mahfudiyah"
+	nupnup.Alamat = "Blitar"
+	nupnup.Umur = 23
 
-	fmt.Println(me)
+	fmt.Println(nupnup)
 
-	fmt.Println(me.Nama)
-	fmt.Println(me.Alamat)
-	fmt.Println(me.Umur)
+	fmt.Println(nupnup.Nama)
+	fmt.Println(nupnup.Alamat)
+	fmt.Println(nupnup.Umur)
+
+	//(2) struct literals
+	Farhan := Customer{
+		Nama:   "Farhan Rizky Alkarim",
+		Alamat: "Tulungagung",
+		Umur:   17,
+	}
+
+	fmt.Println(Farhan)
+
+	//(3) struct literals
+	Pandan := Customer{"Pandan Zahwa Azizah", "Sukoharjo, Solo", 23} // bisa seperti ini yang penting urutan sesuai
+	fmt.Println(Pandan)
 }
