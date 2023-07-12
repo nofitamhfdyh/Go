bb99d966157481d8a2faa3aa24356b346f711135
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-12 08:48:12 +0700

    Lanjutan Struct

--- struct.go
+++ struct.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Customer struct { //utk struct, rata" namanya diawalai dengan huruf kapital. kalau ada 2 kata, brti 2 huruf paling depan kapital
 	Nama, Alamat string
 	Umur         int
@@ -7,8 +9,15 @@ type Customer struct { //utk struct, rata" namanya diawalai dengan huruf kapital
 
 func main() {
 	// mmenggabungkan nol / lebih data yang tipenya beda dalam 1 kesatuan.
-	var nofita Customer
-	nofita.Nama = "Nofita Mahfudiyah"
-	nofita.Alamat = "Blitar"
-	nofita.Umur
+	// sangat cocok utk merepresentasikan data daripada memakai map atau array, karena strukturnya lebih bagus
+	var me Customer
+	me.Nama = "Nofita Mahfudiyah"
+	me.Alamat = "Blitar"
+	me.Umur = 23
+
+	fmt.Println(me)
+
+	fmt.Println(me.Nama)
+	fmt.Println(me.Alamat)
+	fmt.Println(me.Umur)
 }
