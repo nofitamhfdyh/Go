69971f012555f4b343c8f9fcc740d4e1ba19fa85
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-15 23:36:07 +0700

    Akses Variabel dan Function (Huruf Kapital dan tidak kapital)

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,13 @@ package helper
 
 import "fmt"
 
+var version = 1 // (3) tidak bisa diakses krn diawali dgn huruf kecil
+var Application = "Belajar Golang"
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+func sayGoodBye(name string) { // jika function diawali dgn huruf kecil, maka tidak bisa diakses di luar package (2)
+	fmt.Println("Hallo", name)
 }
--- import.go
+++ import.go
@@ -1,9 +1,16 @@
 package main
 
-import "test123/helper"
+import (
+	"fmt"
+	"test123/helper"
+)
 
 func main() {
 	helper.SayHello("Nofita")
-
 	// run output di terminal -> go run import.go
+
+	// helper.sayGoodBye("Nofita") -> jika function diawali dgn huruf kecil, maka tidak bisa diakses di luar package (2)
+	fmt.Println(helper.Application)
+	//fmt.Println(helper.version) -> (3) tidak bisa diakses krn diawali dgn huruf kecil
+
 }
