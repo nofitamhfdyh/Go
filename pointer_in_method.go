45bdeee3cd8f45d1a98de65183549b4ec1406449
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-15 22:30:31 +0700

    Pointer di Method yang belum digunakan

---
+++ pointer_in_method.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Man struct {
+	Nama string
+}
+
+func (man Man) Married() {
+	man.Nama = "Ms. " + man.Nama
+	fmt.Println("DI Method", man.Nama)
+}
+func main() {
+	// walaupun method akan menempel di struct, sebenarnya data struct yang diakses dalam method adalah pass by value
+	// sehingga, direkomendasikan menggunakan pointer di method -> agar tudak boros memory
+
+	nupnup := Man{"Nofita"}
+	nupnup.Married()
+
+	fmt.Println(nupnup.Nama)
+}
