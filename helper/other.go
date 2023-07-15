567dad984ff724720ff20133ee8e63bcb668515a
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-15 23:18:24 +0700

    Package

---
+++ helper/other.go
@@ -0,0 +1,6 @@
+package helper
+
+func SayHello(name string) {
+	//jika ada function yang namanya sama, dan masih berada dalam 1 folder/package -> error / tidak boleh
+	//tapi jika function sama tapi berada dalam folder yang berbeda, tapi masih dalam 1 project -> boleh / tidak error
+}
