dfc7442d14cd230628a2a4a4f4dc84ec24531942
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-16 00:34:42 +0700

    function os Hostname dan Getenv

--- os.go
+++ os.go
@@ -14,4 +14,21 @@ func main() {
 	fmt.Println(args[1]) // output nofita
 	fmt.Println(args[2]) // output mahfudiyah
 
+	//bisa buka di link documentation -> golang.org/pkg/os/
+	hostname, err := os.Hostname()
+	if err == nil {
+		fmt.Println("Hostname : ", hostname) // terminal -> go run os.go nupnup nupnup
+	} else {
+		fmt.Println("Error", err.Error())
+	}
+
+	//get environtment utk koneksi ke database lokal
+	username := os.Getenv("APP_USERNAME")
+	password := os.Getenv("APP_PASSWORD")
+
+	fmt.Println(username)
+	fmt.Println(password)
+	// terminal -> export APP_USERNAME=root
+	// terminal -> export APP_PASSWORD=root
+	// utk cek bisa langsung ke -> go run os.go
 }
