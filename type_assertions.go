4a8f473573b5423ec02a748b4cf9fab43981ab91
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-15 19:48:36 +0700

    Type Assertions with Switch

--- type_assertions.go
+++ type_assertions.go
@@ -3,15 +3,24 @@ package main
 import "fmt"
 
 func random() interface{} { //returnya adalah interface kosong
-	return "OK"
+	return 100
 }
 
 func main() {
 	//merubah tipe data sesuai dengan yang kita inginkan, digunakan saat bertemu dengan interface kosong
+	//agar mengurangi risiko panic dalam type assertions, maka digunakan penggunaan switch dalam implementasinya
 
 	result := random()
-	resultString := result.(string) // melakukan konversi dari interface kosong menjadi string
-	fmt.Println(resultString)
+	//resultString := result.(string) // melakukan konversi dari interface kosong menjadi string
+	//fmt.Println(resultString)
+	switch value := result.(type) {
+	case string:
+		fmt.Println("String", value)
+	case int:
+		fmt.Println("Int", value)
+	default:
+		fmt.Println("Unknown Type")
+	}
 
 	//panic, cth konversi yang tidak sesuai
 	//resultInt := result.(int)
