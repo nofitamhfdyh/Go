e579b4186ed1f4c3f153df55b1ad1fb484b633fe
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-15 20:20:32 +0700

    Operator *

--- pointer.go
+++ pointer.go
@@ -9,10 +9,15 @@ type Address struct {
 func main() {
 	//(1)Pass by Value -> jika kita mengirim variabel ke sebuah function / method / variable lain, yang dikirim adalah duplikasi valuenya. sudah by default dari Golang
 	//konsep di bahasa pemrograman lain menggunakan pass by referemce
+
 	// Pointer -> kemampuan yang digunakan utk membuat reference ke lokasi data di memory yang sama tanpa mendupilkasi data yang sudah ada.
 	// membuat pass by reference menggunakan pointer
 	// cara membuat pointer itu menggunakan operator & (and)
 	// (2) operator & -> membuat variable dengan nilai pointer ke variable yang lain -> operator & diikuti dengan nama variable nya
+
+	//saat mengubah variabel pointer, berarti yang berubah hanya variabel
+	// seluruh variabel yang mengacu ke data yang sama tidak akan berubah
+	// jika ingin mengubah seluruh variable yang mengacu ke data tsb, menggunakan --> operator * (3)
 
 	//(1)
 	address1 := Address{
@@ -22,9 +27,18 @@ func main() {
 	}
 	//address2 := address1 // (1)
 	address2 := &address1
+	address3 := &address1
 
 	address2.Kota = "Surabaya"
 
+	//(3)
+	*address2 = Address{ //tambah bintang didepan variabel, sehingga address 1 dipaksa untuk mengikuti isi dari address 2
+		Kecamatan: "Pasar Minggu",
+		Kota:      "Jakarta Selatan",
+		Provinsi:  "DKI Jakarta",
+	}
+
 	fmt.Println(address1) //tidak berubah isinya, tetap -> ini yang dimaksud dengan pass by value (1)
 	fmt.Println(address2) //didepannya jadi ada & -> memberitahu bahwa kalau address 2 itu pointer -> cara cek nya klik ctrl + spasi
+	fmt.Println(address3)
 }
