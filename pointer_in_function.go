4f26327aca06fe8784e8c210d26c20d0a5097e3f
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-15 20:39:04 +0700

    Pointer di FUnction (data tidak berubah)

---
+++ pointer_in_function.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Alamat struct {
+	Kota, Provinsi, Negara string
+}
+
+func ChangeAddressToIndonesia(address Alamat) {
+	address.Negara = "Indonesia"
+}
+func main() {
+	// secara default -> pass by value
+	// maka, jika mengubah data dlm function, data asli tidak akan pernah berubah -> variable nya aman krn tdk bisa dirubah
+	// namun saat tertentu, kita ingin membuat function yang bisa mengubah data asli parameter tsb -> dilakukan pointer di function
+	// utk menjadikan sebuah parameter sbg pointer, bisa menggunakan operator * di parameternya
+
+	addres := Alamat{
+		Kota:     "Blitar",
+		Provinsi: "Jawa Timur",
+		Negara:   "",
+	}
+	ChangeAddressToIndonesia(addres)
+
+	fmt.Println(addres) // tidak berubah, karena yang dikirim adalah data copyan value nya
+}
